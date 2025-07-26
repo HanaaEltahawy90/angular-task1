56a9cd24ee900b2f4cb4093f1c125c6812bb35f5
Hanaa Eltahawy <hanaaeltahawy450@gmail.com>
2025-07-26 22:33:52 +0300

    Completed task

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes , withViewTransitions()), provideClientHydration(withEventReplay())
+  ]
+};
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css'
+})
+export class PortfolioComponent {
+
+  isHidden:boolean =true;
+modelImg:string ='';
+
+imgsrc:string[]=[
+  '/img/poert1.png',
+  '/img/port2.png',
+  '/img/port3.png',
+  '/img/poert1.png',
+  '/img/port2.png',
+  '/img/port3.png',
+];
+
+
+
+
+
+}
+  
+
+
