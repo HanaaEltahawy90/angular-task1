f52fdef0766be10e16942a98d6b42bc522a4c733
HanaaEltahawy90 <hanaa.emad.eltahawy1@gmail.com>
2025-07-28 17:26:44 +0300

    update

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,30 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+  styleUrl: './portfolio.component.css',
 })
 export class PortfolioComponent {
+    @HostListener('window:click', ['$event'])
 
-  isHidden:boolean =true;
-modelImg:string ='';
+  isHidden: boolean = true;
+  flag=true;
+  modelImg: string = '';
 
-imgsrc:string[]=[
-  '/img/poert1.png',
-  '/img/port2.png',
-  '/img/port3.png',
-  '/img/poert1.png',
-  '/img/port2.png',
-  '/img/port3.png',
-];
+  imgsrc: string[] = [
+    '/img/poert1.png',
+    '/img/port2.png',
+    '/img/port3.png',
+    '/img/poert1.png',
+    '/img/port2.png',
+    '/img/port3.png',
+  ];
+  openModal(event: MouseEvent, src: string) {
+  event.stopPropagation(); 
+  this.modelImg = src;
+  this.isHidden = false;
+}
 
-
-
-
+@HostListener('window:click', ['$event'])
+onWindowClick(event: MouseEvent) {
+  this.isHidden = true;
+}
 
 }
-  
-
-
